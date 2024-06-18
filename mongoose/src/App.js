f9ead089c7652eb6e6b1729cc3c1340df2e160bb
shreyaskar-1 <shreyaskar_219031@gmail.com>
2024-06-18 12:07:08 +0530

    Changes

--- mongoose/src/App.js
+++ mongoose/src/App.js
@@ -65,15 +65,76 @@ const createDocument = async() => {
 // read document and querying in it
 const readDocument = async() => {
   try{
-    const result = await Test.find({age:21})
-    .select({name:1})
-    .limit(1);
+    const result = await Test
+    // using logical operator in querying
+    // or operator performs logicalor operation on an array of two or more expression and selects the documents that satisfy at keast one of the expression.
+    // .find({
+    //   $or: [
+    //     { profile: { $in: [ "Web Designer"] } }
+    //     , { age: { $gt: 20 } }
+    //   ]
+    // })
+
+    // and operator 
+    // .find({
+    //   $and: [
+    //     { profile: { $in: [ "Web Designer", "Full Stack Developer"] } }
+    //     , { age: { $gt: 20 } }
+    //   ]
+    // })
+    
+    // nor opertaor
+    .find({
+      $nor: [
+        { profile: { $in: [ 'Full Stack Developer', 'Web Designer'] } }
+        , { age: { $gt: 30 } }
+      ]
+    })
+
+    // using count : used to count the document of the database.
+    // .countDocuments()
+
+    // sort method
+    .sort({name:1})
+
+    // using in operator that gives output if it matches any value specified in th array
+    // we can also use nin for not in 
+    // .find({profile:{$in:["Full Stack Developer", "Web Designer"]}})
+
+    // gt refers to greater than and if you put and e it denotes equal to  . also it is same for less than
+    // .find({age:{$gte:22}})
+
+    // used to normally read the data from database 
+    // .find({age:21}) 
+
+    // select is used to select only one field of the whole databse
+    // .select({name:1})
+
+    // limit helps to put an limit to the database fields
+    // .limit(1);
+
     console.log(result);
   }catch(err){
     console.log(err);
   }
 }
 // readDocument();
+
+// Updating the Document using updateOne or updateMany
+// we can also use findByIdAndUpdate by putting useFindAndModify to false .
+const updateDocument = async (_id) =>{
+  try{
+    const res = await Test.findByIdAndUpdate({_id},{$set:{name:"Lakshya"}},
+      {
+        new:true
+      }
+    );
+    console.log(res);
+  }catch(err){
+    console.log(err);
+  }
+}
+updateDocument("666de1fa98a39f3423781697")
 
 
 
